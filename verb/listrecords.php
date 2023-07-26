f25838e408e77c6774ed3f4a987ef140a16e5fd3
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-07-26 08:23:59 +0700

    clean up

--- verb/listrecords.php
+++ verb/listrecords.php
@@ -28,8 +28,11 @@ $next_resumption_token = $resumptionToken->next_resumption_token;
 
 /**
  * Ini adalah lokasi file dimana masing2 data katalog di query dan disusun output untuk Dublin Core nya  
+ * 
  * NOTE : Karena file2 ini di include, maka variabel2 yang ada diatas maupun yg ada di index.php
- *        semuanya dapat diakses langsung di masing2 file data katalog
+ *        semuanya dapat diakses langsung di masing2 file data katalog,
+ *        jika menemukan variabel di komponen yang tiba2 ada tanpa didefinisikan terlebih dulu, maka
+ *        coba cari di file ini atau di index.php
  */
 include_once "component/listrecords/buku.php";
 
